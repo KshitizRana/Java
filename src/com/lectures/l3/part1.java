d0692d28fe57c0c4bfa481ad31717f77a0fd0bcc
Kaustubha Pundir <kaustubhapundir@gmail.com>
2025-02-19 12:22:59 +0530

    added the l3-: part1 & part2

---
+++ src/com/lectures/l3/part1.java
@@ -0,0 +1,26 @@
+package com.lectures.l3;
+
+import java.util.Scanner;
+
+public class part1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int [] arr={1,2,3,4,5,6};
+        System.out.println("Enter the number to search");
+        int n=sc.nextInt();
+        boolean isfound=isFound(arr,n);
+        if (isfound){
+            System.out.println("Found");
+        } else{
+            System.out.println("Not Found");
+        }
+    }
+    static boolean isFound(int [] arr,int n){
+        for (int i=0;i<arr.length;i++){
+            if (arr[i]==n){
+                return true;
+            }
+        }
+        return false;
+    }
+}
