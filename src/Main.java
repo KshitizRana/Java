1e32e3185b9cdfc5eac795928b6c582e23db9833
Kaustubha Pundir <kaustubhapundir@gmail.com>
2025-02-19 07:36:42 +0530

    added Lecture 2's code

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        L2_1();
+    }
+    static void L2_1(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Year: ");
+        int input=sc.nextInt();
+        if(input%4==0 && (input%100!=0 || input%400==0)){
+            System.out.println(input+" is a leap year");
+        }
+        else {
+            System.out.println(input+" is not a leap year");
+        }
+    }
+}
