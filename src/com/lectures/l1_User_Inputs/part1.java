6f437b549f148e213a6a4a4cf7b348391894dc5e
Kshitiz Rana <ranakshitizgm@gmail.com>
2025-02-22 12:32:27 +0530

    Update userInputs

--- src/com/lectures/l1_User_Inputs/part1.java
+++ src/com/lectures/l1_User_Inputs/part1.java
@@ -1,7 +1,12 @@
 package com.lectures.l1_User_Inputs;
 
+import java.util.Scanner;
+
 public class part1 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your Name: ");
+        String name = in.next();
+        System.out.println("hello! "+name);
     }
 }
